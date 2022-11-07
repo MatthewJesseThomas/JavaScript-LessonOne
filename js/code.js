8c2bcc814772c03c33c43cf677d94d150352acef
MatthewJesseThomas <owlsaintgermain@gmail.com>
2022-11-07 10:28:30 +0200

    main

---
+++ js/code.js
@@ -0,0 +1,10 @@
+// function madeBy() {
+//     alert("Made By Shishutemu Toomasu Mashu");
+// }
+let madeBy = document.querySelector('button');
+// Add listener
+madeBy.addEventListener('click', ()=>{
+    alert("Made By Shishutemu Toomasu Mashu");
+});
+
+
